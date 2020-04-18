7eeb79540c448209638db7674e9bef900abe12b4
Jake <50179520+jtweedle@users.noreply.github.com>
2020-04-18 13:22:07 -0500

    Delete array_Tweedle.m

--- array_Tweedle.m
+++
@@ -1,8 +0,0 @@
-%this function calculates sum of integer array
-function sum = array_Tweedle(numArray)
-    sum = 0;
-    for i = 1:length(numArray)
-        sum = sum +numArray(i);
-    end
-end
-        
